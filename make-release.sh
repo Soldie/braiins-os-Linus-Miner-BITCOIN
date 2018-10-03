3890eecdc0fdf32a44263a581a629013ae711c80
Jan Čapek <jan.capek@braiins.cz>
2018-10-03 19:21:38 +0200

    Remove platform specification when creating a release
    - adjust make-release.sh to not specify particular platform as the
      command now performs release for all known platforms

--- make-release.sh
+++ make-release.sh
@@ -6,7 +6,7 @@
 #
 #
 
-# Synopsis: ./make-release.sh SUBTARGET1 [SUBTARGET2 [SUBTARGET3...]]
+# Synopsis: ./make-release.sh
 
 target=zynq
 release_subtargets=$@
@@ -15,9 +15,4 @@ $DRY_RUN virtualenv --python=/usr/bin/python3.5 .env
 $DRY_RUN source .env/bin/activate
 $DRY_RUN pip3 install -r requirements.txt
 
-# Create release for all subtargets
-for subtarget in $release_subtargets; do
-    platform=$target-$subtarget
-    echo Releasing $platform
-    $DRY_RUN ./bb.py --platform $platform release
-done
+$DRY_RUN ./bb.py release
