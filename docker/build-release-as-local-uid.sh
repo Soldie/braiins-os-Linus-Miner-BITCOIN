1217a6c51705d67ca8562c37334f579efa89658f
Jan Čapek <jan.capek@braiins.cz>
2018-10-19 17:15:04 +0200

    [docker] remove release-build directory as build result goes to 'output' directory
    - remove owner change as it is caller's responsibility to have this
      setup properly

--- docker/build-release-as-local-uid.sh
+++ docker/build-release-as-local-uid.sh
@@ -3,8 +3,7 @@
 
 addgroup --gid=$LOC_GID build
 adduser --system --home=$RELEASE_BUILD_DIR --no-create-home --uid=$LOC_UID --gid=$LOC_GID build
-# release build is already created due to the fact that we have mapped into it .ssh with known hosts file
-chown build.build $RELEASE_BUILD_DIR
+
 sudo -H -E -u build bash << EOF
 ./build-release.sh $@
 EOF
