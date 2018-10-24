9ef8b9f051961e8982f09c3cc2bf1ac99aeb4364
Jan Čapek <jan.capek@braiins.cz>
2018-10-24 07:34:10 +0200

    [docker] Run the build in interactive mode to show more build progress info

--- docker/build-release-in-docker.sh
+++ docker/build-release-in-docker.sh
@@ -2,4 +2,4 @@
 
 release_build_dir=/src
 docker run --env RELEASE_BUILD_DIR=$release_build_dir --env LOC_UID=`id -u` --env LOC_GID=`id -g` --volume $HOME/.ssh/known_hosts:$release_build_dir/.ssh/known_hosts:ro --volume $SSH_AUTH_SOCK:/ssh-agent --volume\
- ${PWD}:/src -w /src --env SSH_AUTH_SOCK=/ssh-agent braiins-os-builder ./docker/build-release-as-local-uid.sh $@
+       ${PWD}:/src -w /src --env SSH_AUTH_SOCK=/ssh-agent -ti braiins-os-builder docker/build-release-as-local-uid.sh $@
